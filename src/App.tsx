7b12449c6d97c56e83ecbfc3cbb4a1fc9627f92e
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-13 23:02:51 -0300

    feat: add router to new Steps components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import "./styles/global.css";
 import "./styles/theme.css";
 
 import { Home } from "./pages/Home";
-import { Steps } from "./components/Steps";
+import { Steps } from "./pages/Steps";
+import { Steps2 } from "./pages/Steps2";
+import { Steps3 } from "./pages/Steps3";
 
 export function App() {
   return (
@@ -12,8 +14,8 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/steps" element={<Steps />} />
-        <Route path="/steps/2" element={<Steps />} />
-        <Route path="/steps/3" element={<Steps />} />
+        <Route path="/steps/2" element={<Steps2 />} />
+        <Route path="/steps/3" element={<Steps3 />} />
       </Routes>
     </Router>
   );
