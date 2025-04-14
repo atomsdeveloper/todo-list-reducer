a406d91d83a6428bfb217691dec7ad96f90ffedf
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-13 21:39:09 -0300

    fix: remove component useless

--- src/components/ContainerSteps/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css";
-
-type ContainerStepsTypes = {
-  children: React.ReactNode;
-};
-
-export function ContainerSteps({ children }: ContainerStepsTypes) {
-  return <div className={styles.container}>{children}</div>;
-}
