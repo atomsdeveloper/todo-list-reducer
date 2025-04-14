08cf92d3b3dc0eb78e6a1766862926347c4e6852
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-14 20:59:54 -0300

    feat: passing values to steps

--- src/pages/Steps2/index.tsx
+++ src/pages/Steps2/index.tsx
@@ -1,5 +1,12 @@
 import { Steps } from "../Steps";
 
 export function Steps2() {
-  return <Steps />;
+  return (
+    <Steps
+      title="Create daily routine"
+      description="In Uptodo  you can create your personalized routine to stay productive"
+      alt="Create daily routine"
+      image="/images/create-routine.png"
+    />
+  );
 }
--- src/pages/Steps3/index.tsx
+++ src/pages/Steps3/index.tsx
@@ -1,5 +1,12 @@
 import { Steps } from "../Steps";
 
 export function Steps3() {
-  return <Steps />;
+  return (
+    <Steps
+      title="Orgonaize your tasks"
+      description="You can organize your daily tasks by adding your tasks into separate categories"
+      alt="Orgonaize your tasks"
+      image="/images/organize-task.png"
+    />
+  );
 }
