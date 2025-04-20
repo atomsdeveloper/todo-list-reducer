f4a6e9a19ee354cc1bb54ce1f0442cc303df0550
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-19 21:21:36 -0300

    feat: add router to Login component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./styles/theme.css";
 import { Home } from "./pages/Home";
 import { Steps } from "./pages/Steps";
 import { StepsContextProvider } from "./context/StepsContext/StepsContextProvider";
+import { Login } from "./pages/Login";
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/steps" element={<Steps />} />
+          <Route path="/steps/login" element={<Login />} />
         </Routes>
       </Router>
     </StepsContextProvider>
