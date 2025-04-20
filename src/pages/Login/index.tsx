28995a05e6e619176a62a3e826ff8849e7b6ab70
Renan Nascimento <atomsfrontend@gmail.com>
2025-04-19 21:21:56 -0300

    feat: created Login component

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../../components/Button";
+
+export function Login() {
+  return (
+    <div>
+      <Button text="<" size="md" />
+      <p>Criando tela de login</p>
+      <h1>Teste</h1>
+    </div>
+  );
+}
